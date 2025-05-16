f7560c41354ad0e35d369c4b294d548577d5d18c
qa-jahidul <qa.jahidul@gmail.com>
2025-05-16 19:19:53 +0600

    HandleBrowserTask Added

---
+++ src/test/java/HandleBrowserRelatedTask.java
@@ -0,0 +1,17 @@
+import org.testng.annotations.Test;
+
+public class HandleBrowserRelatedTask extends DriverSetup{
+    @Test
+    public void testBrowserTask() throws InterruptedException {
+        driver.get("https://www.google.com/");
+
+        String title = driver.getTitle();
+        System.out.println("Title: " + title);
+
+        String url = driver.getCurrentUrl();
+
+        System.out.println("URL: "+ url);
+
+       Thread.sleep(3000);
+    }
+}
